b4e5a885a486222b27a221808136dcbc507bae4c
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2023-12-10 21:18:27 +0200

    commit

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import { Overlay, ModalWindow } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ largeImageURL, tags, onClose }) => {
+    useEffect(() => {
+        const handleKeyDown = event => {
+            if (event.code === 'Escape') {
+                onClose();
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        document.body.style.overflow = 'hidden';
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+            document.body.style.overflow = 'visible';
+        };
+    }, [onClose]);
+
+    const handleBackdropClick = event => {
+        if (event.currentTarget === event.target) {
+            onClose();
+        }
+    };
+
+    return createPortal(
+        <Overlay onClick={handleBackdropClick}>
+            <ModalWindow>
+                <img src={largeImageURL} alt={tags} />
+            </ModalWindow>
+        </Overlay>,
+        modalRoot
+    );
+};
+
+Modal.propTypes = {
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+    onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect, useCallback } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import PropTypes from 'prop-types';
+import {
+    SearchForm,
+    SearchInput,
+    SearchButton,
+    SearchSpan,
+} from './SearchBar.styled';
+
+const SearchBar = ({ onSubmit }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const searchQuery = inputValue.trim();
+
+        if (searchQuery !== '') {
+            onSubmit(searchQuery);
+            setInputValue('');
+        }
+    };
+
+    const handleKeyPress = useCallback(
+        (event) => {
+            if (event.key === 'Enter') {
+                handleSubmit(event);
+            }
+        },
+        [handleSubmit]
+    );
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress);
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleKeyPress]);
+
+    return (
+        <header>
+            <SearchForm onSubmit={handleSubmit}>
+                <SearchButton>
+                    <BsSearch />
+                    <SearchSpan>Search</SearchSpan>
+                </SearchButton>
+                <SearchInput
+                    name="searchName"
+                    type="text"
+                    id="search"
+                    value={inputValue}
+                    onChange={handleChange}
+                />
+            </SearchForm>
+        </header>
+    );
+};
+
+SearchBar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
